5fdc7e429535ce7773040620a8697d9c2e1afb6c
martolod <Ad112358>
2013-11-10 00:03:34 +0400

    Некоторые подвижки в области нейросетей.
    Производится активное дополнительно чтение.

--- module.py
+++ module.py
@@ -1,6 +1,36 @@
 from NodesLibrary import *
 from OwnMath import *
 import random
+from math import log
+from pybrain.tools.shortcuts import buildNetwork
+from pybrain.datasets import SupervisedDataSet
+from pybrain.supervised.trainers import BackpropTrainer
+
+
+class OwnNeuro():
+    def __init__(self, input_power, output_power, education_power):
+        self.input_power = input_power
+        self.output_power = output_power
+        self.education_set_length = education_power * PARTITION_OF_EDUCATION_VERIFICATION_SET
+        self.data_set = SupervisedDataSet(self.input_power, self.output_power)
+        self.hidden_power = int(
+            (output_power * education_power / (1 + log(education_power, 2))) / (input_power + output_power)) + 2
+        self.network = buildNetwork(self.input_power, self.hidden_power, self.output_power)
+
+    def _form_set(self, input_row, output_row):
+        for one_portion in range(self.education_set_length):
+            input_tuple = ()
+            for one_input in range(self.input_power):
+                input_tuple += (input_row[one_input][one_portion],)
+            output_tuple = ()
+            for one_output in range(self.output_power):
+                output_tuple += (output_row[one_output][one_portion],)
+            self.data_set.addSample(input_tuple, output_tuple)
+
+    def educate(self, input_row, output_row):
+        self._form_set(input_row, output_row)
+        trainer = BackpropTrainer(self.network, self.data_set)
+
 
 
 class OneTree():
